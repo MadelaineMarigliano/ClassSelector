e3e424ee2adbc5a6f385b0d0950fe71d41e83319
madelainemarigliano <madelaine.marigliano@mail.utoronto.ca>
2021-04-21 15:18:02 -0400

    adding files

--- src/ScheduleManager.java
+++ src/ScheduleManager.java
@@ -23,6 +23,7 @@ public class ScheduleManager {
     }
 
     //These courses are the ones with preferences applied already
+    //We should probably use recursion here... sbatessss
     //NOT DONE
     private ArrayList<Schedule> createSchedules(ArrayList<Course> courses){
 
