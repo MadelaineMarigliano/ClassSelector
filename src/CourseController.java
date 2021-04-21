23f2db7734a8fb52ef1d0614438c43059de6624a
Jasmeen Sran <jasmeen.sran@mail.utoronto.ca>
2021-04-21 16:03:53 -0400

    l

--- src/CourseController.java
+++ src/CourseController.java
@@ -7,5 +7,26 @@ public class CourseController {
         Scanner scanner = new Scanner(System.in);
         view.displayMenu();
         int option;
+        option = selectOption(scanner);
+    }
+
+    private int selectOption(Scanner scanner) {
+        String input;
+        do {
+            view.optionPrompt();
+            input = scanner.nextLine();
+            switch(input.trim()) {
+                case "1":
+                    return 1;
+                case "2":
+                    return 2;
+                case "3":
+                    return 3;
+                case "4":
+                    return 4;
+            }
+            view.optionError();
+            } while(true);
+        }
     }
 }
