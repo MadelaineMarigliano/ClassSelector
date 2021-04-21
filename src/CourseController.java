70106894c351abd7da4be553907d6efd2cc4d5ad
madelainemarigliano <madelaine.marigliano@mail.utoronto.ca>
2021-04-21 15:24:03 -0400

    adding files

---
+++ src/CourseController.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CourseController {
+    private CourseView view;
+
+    private void Run() {
+        Scanner scanner = new Scanner(System.in);
+        view.displayMenu();
+        int option;
+    }
+}
